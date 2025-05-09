8d873eb29c6e732600bcc39c5759e4de1b072c3e
Eric Schaefer <eric.schaefer@seven.one>
2025-05-09 11:39:21 +0200

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,15 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
   adapter: cloudflare(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
