70e6adca62d86b51315a432d0c15923a1395a3b7
Eric Schaefer <eric.schaefer@seven.one>
2025-05-16 15:03:23 +0200

    more changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,19 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://eric-schaefer.com',
+  experimental: {
+    responsiveImages: true,
+  },
+  image: {
+    experimentalLayout: 'constrained',
+  },
   integrations: [mdx(), sitemap()],
-  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()],
   },
