9a89797bd1d04b28c4df9c248a6c63e95bc766f7
Eric Schaefer <omg@eric-schaefer.com>
2025-05-23 11:34:08 +0200

    paddings

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Eric Schaefer - software // music // nonsense';
-export const SITE_DESCRIPTION = 'Eric Schaefer is a software engineer in Berlin.';
+export const NAME = 'Eric Schaefer';
+export const SITE_TITLE = `${NAME} - software // music // nonsense`;
+export const SITE_DESCRIPTION = `${NAME} is a software engineer in Berlin.`;
